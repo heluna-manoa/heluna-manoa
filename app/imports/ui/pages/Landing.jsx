a83a85498f0a19c3ed5b61c3a02284451b09035e
KurtKimura02 <kurtkimu@hawaii.edu>
2023-11-21 11:38:32 -1000

    send to specific link page (not working)

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function courseSearch(name) {
+  // go through all courses, if course exists
+  if (name === course.name) {
+    return `coursereview/${course.name}`;
+  }
+  return 'searchcourses/';
+
+  // send to course page
+  // else send to list with search query
+}
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => {
@@ -9,6 +21,12 @@ const Landing = () => {
   const { currentUser } = useTracker(() => ({
     currentUser: Meteor.user() ? Meteor.user().username : '',
   }), []);
+
+  function useState(something) {
+    return something;
+  }
+
+  const [query, setQuery] = useState('');
 
   return (
     <Container id="landing-page" fluid className="py-3">
@@ -19,7 +37,30 @@ const Landing = () => {
         <Col>
           <h2>Search by Course</h2>
           <form method="get" action="/searchcourse">
-            <input type="search" id="course-search" name="course" />
+            <input type="text" id="course-search" name="course" />
+          </form>
+          <form>
+            <input
+              id="searchInput"
+              className="focus:outline-none"
+              type="text"
+              placeholder="Search"
+              value={query}
+              onChange={(event) => {
+                setQuery(event.target.value);
+              }}
+            />
+            <div className="flex mt-1.5">
+              <Link to={{ pathname: `/coursereview/${query}` }}>
+                <button type="submit" onClick={() => setQuery(() => '')}>
+                  <svg
+                    className="fill-current h-auto w-4 "
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  />
+                </button>
+              </Link>
+            </div>
           </form>
         </Col>
         <Col>
