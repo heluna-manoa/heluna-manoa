21178cff6884700cca578f1980509f3d39e036c2
Ryan <ryanseng32@gmail.com>
2023-11-21 11:36:35 -1000

    Edit Review now returns to UserReviews after submit

--- app/imports/ui/pages/EditReview.jsx
+++ app/imports/ui/pages/EditReview.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import swal from 'sweetalert';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { AutoForm, ErrorsField, LongTextField, SelectField, SubmitField } from 'uniforms-bootstrap5';
@@ -9,10 +9,11 @@ import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { useParams } from 'react-router';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Reviews } from '../../api/reviews/Review';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 /* Renders the EditContact page for editing a single document. */
 const EditReview = () => {
+  const [redirect, setRedirect] = useState(false);
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
   const { _id } = useParams();
   // console.log('EditStuff', _id);
@@ -54,11 +55,18 @@ const EditReview = () => {
   // On successful submit, insert the data.
   const submit = (data) => {
     const { courseName, reviewContent, rating, grade } = data;
-    Reviews.collection.update(_id, { $set: { courseName, reviewContent, rating, grade } }, (error) => (error ?
-      swal('Error', error.message, 'error') :
-      swal('Success', 'Review updated successfully', 'success')));
+    Reviews.collection.update(_id, { $set: { courseName, reviewContent, rating, grade } }, (error) => {
+      if (error) {
+        swal('Error', error.message, 'error');
+      } else {
+        swal('Success', 'Review updated successfully', 'success');
+        setRedirect(true);
+      }
+    });
   };
-
+  if (redirect) {
+    return (<Navigate to="/userreviews" />);
+  }
   return ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
