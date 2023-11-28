0a65a6aebe295427fcf46c0cb0aa17dcd164287e
KurtKimura02 <kurtkimu@hawaii.edu>
2023-11-28 10:18:21 -1000

    Create ProfessorCard.jsx

---
+++ app/imports/ui/components/ProfessorCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
+
+const displayStars = (review) => {
+  const stars = [];
+  for (let i = 0; i < review.rating; i++) {
+    stars.push(<span> ★ </span>);
+  }
+  return stars;
+};
+
+const ProfessorCard = ({ review }) => (
+  <Card className="h-100 landing-card">
+    <Card.Header>
+      <Card.Title> {displayStars(review)} <br /> {review.professor}
+      </Card.Title>
+      <Card.Subtitle>{review.courseName}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{review.reviewContent}</Card.Text>
+      <Card.Subtitle>
+        Grade: {review.grade} <br /> <i>{review.reviewer}</i>
+      </Card.Subtitle>
+    </Card.Body>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+ProfessorCard.propTypes = {
+  review: PropTypes.shape({
+    courseName: PropTypes.string,
+    professor: PropTypes.string,
+    reviewContent: PropTypes.string,
+    rating: PropTypes.number,
+    grade: PropTypes.string,
+    reviewer: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default ProfessorCard;
