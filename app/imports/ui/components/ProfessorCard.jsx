8b563c990fcdd78cb3441600cbaf8894fdfb3ef8
KurtKimura02 <kurtkimu@hawaii.edu>
2023-12-04 16:17:48 -1000

    updated review to professor in professorcard

--- app/imports/ui/components/ProfessorCard.jsx
+++ app/imports/ui/components/ProfessorCard.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
 
-const displayStars = (review) => {
+const displayStars = (professor) => {
   const stars = [];
-  for (let i = 0; i < review.rating; i++) {
+  for (let i = 0; i < professor.rating; i++) {
     stars.push(<span> ★ </span>);
   }
   return stars;
 };
 
-const ProfessorCard = ({ review }) => (
+const ProfessorCard = ({ professor }) => (
   <Card className="h-100 landing-card">
     <Card.Header>
       <Card.Title>
-        <Image src={review.image} />
-        {review.professor}
+        <Image src={professor.image} />
+        {professor.professor}
         <br />
-        {displayStars(review)}
+        {displayStars(professor)}
       </Card.Title>
-      <Card.Subtitle>{review.department}</Card.Subtitle>
+      <Card.Subtitle>{professor.department}</Card.Subtitle>
     </Card.Header>
     <Card.Body>
-      <Card.Text>{review.courses}</Card.Text>
+      <Card.Text>{professor.courses}</Card.Text>
     </Card.Body>
   </Card>
 );
 
 // Require a document to be passed to this component.
 ProfessorCard.propTypes = {
-  review: PropTypes.shape({
+  professor: PropTypes.shape({
     professor: PropTypes.string,
     department: PropTypes.string,
     image: PropTypes.string,
